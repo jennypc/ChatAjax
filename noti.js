d1af5cc8a9b5cfad7382adb4fcf7ca9f62b57a08
jennypc <36174275+jennypc@users.noreply.github.com>
2022-06-23 17:59:31 -0500

    h

--- noti.js
+++ noti.js
@@ -9,23 +9,17 @@ button.addEventListener('click',function(){
 function noti(){
     
     if (Notification) {
-if (Notification.permission !== "granted") {
-Notification.requestPermission()
-}
-var title = "Xitrus"
-var extra = {
-icon: "http://xitrus.es/imgs/logo_claro.png",
-body: "Notificación de prueba en Xitrus"
-}
-var noti = new Notification( title, extra)
-noti.onclick = {
-// Al hacer click
-}
-noti.onclose = {
-// Al cerrar
-}
-setTimeout( function() { noti.close() }, 10000)
-}
+        if (Notification.permission !== "granted") {
+            Notification.requestPermission();
+        }
+            var title = "Xitrus"
+            var extra = {
+                icon: "http://xitrus.es/imgs/logo_claro.png",
+                body: "Notificación de prueba en Xitrus"
+                }
+        var noti = new Notification( title, extra)
+        setTimeout( function() { noti.close() }, 10000)
+        }
     
 }
 //    
@@ -54,44 +48,7 @@ setTimeout( function() { noti.close() }, 10000)
 //        });
 //    }
 //}
-    
-//      //Vamos a comprobar si el navegador es compatible con las notificaciones
-//  if (!("Notification" in window)) {
-//    alert("This browser does not support desktop notification");
-//  }
-//  // Vamos a ver si ya se han concedido permisos de notificación
-//  else if (Notification.permission === "granted") {
-//    // Si está bien vamos a crear una notificación
-//    var body = "Hola";
-//    var icon = "https://www.quecodigo.com/img/qc_logo.jpg";
-//    var title = "Notificación";
-//    var options = {
-//      body: body,
-//      icon: icon,
-//      lang: "ES",
-//      renotify: "true"
-//    }
-//    var notification = new Notification(title,options);
-//    var audio = new Audio('https://www.quecodigo.com/sounds/notificacion.mp3');
-//    audio.play();
-//    notification.onclick = function () {
-//      //action
-//    };
-//    setTimeout(notification.close.bind(notification), 5000);
-//  }
-//  // De lo contrario, tenemos que pedir permiso al usuario
-//  else if (Notification.permission !== 'denied') {
-//    Notification.requestPermission(function (permission) {
-//      // Si el usuario acepta, vamos a crear una notificación
-//      if (permission === "granted") {
-//        var notification = new Notification("Gracias, Ahora podras recibir notifiaciones de nuestra página");
-//      }
-//    });
-//  }
-//  // Por fin, si el usuario ha denegado notificaciones, y usted
-//  // Quiere ser respetuoso no hay necesidad de preocuparse más sobre ellos.
-//}
-    
+ 
     
 
 
